7b934da0993eba8f4b6878bfd7472f0ff4b6a4a1
Sohbet <maksadow.sohbet97@gmail.com>
2026-05-24 21:46:52 +0300

    added favorite logic

---
+++ lib/feature/favorite/bloc/favorite_event.dart
@@ -0,0 +1,39 @@
+part of 'favorite_bloc.dart';
+
+sealed class FavoriteEvent extends Equatable {
+  const FavoriteEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LoadFavorites extends FavoriteEvent {
+  const LoadFavorites();
+}
+
+final class AddFavoriteProduct extends FavoriteEvent {
+  final ProductModel product;
+
+  const AddFavoriteProduct(this.product);
+
+  @override
+  List<Object> get props => [product.id];
+}
+
+final class RemoveFavoriteProduct extends FavoriteEvent {
+  final int productId;
+
+  const RemoveFavoriteProduct(this.productId);
+
+  @override
+  List<Object> get props => [productId];
+}
+
+final class ToggleFavoriteProduct extends FavoriteEvent {
+  final ProductModel product;
+
+  const ToggleFavoriteProduct(this.product);
+
+  @override
+  List<Object> get props => [product.id];
+}
