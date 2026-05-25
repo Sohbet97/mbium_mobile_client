e3c0be0874adc6d0cc5c2cba99ab547f28f7f1ec
Sohbet <maksadow.sohbet97@gmail.com>
2026-05-25 14:49:17 +0300

    added feature chats

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -2,56 +2,77 @@
 // This is a library that provides messages for a en locale. All the
 // messages from the main program should be duplicated here with the same
 // function name.
-// @dart=2.12
+
 // Ignore issues from commonly used lints in this file.
-// ignore_for_file:unnecessary_brace_in_string_interps
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
-// ignore_for_file:unused_import, file_names
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
 
 import 'package:intl/intl.dart';
 import 'package:intl/message_lookup_by_library.dart';
 
-final messages = MessageLookup();
+final messages = new MessageLookup();
 
-typedef String? MessageIfAbsent(
-    String? messageStr, List<Object>? args);
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 
 class MessageLookup extends MessageLookupByLibrary {
-  @override
   String get localeName => 'en';
 
-  @override
-  final Map<String, dynamic> messages = _notInlinedMessages(_notInlinedMessages);
-
-  static Map<String, dynamic> _notInlinedMessages(Object? _) => {
-      'Ai_agent': MessageLookupByLibrary.simpleMessage('AI agent'),
-    'AppName': MessageLookupByLibrary.simpleMessage('Mbium'),
-    'add': MessageLookupByLibrary.simpleMessage('Add'),
-    'ai_agendyn': MessageLookupByLibrary.simpleMessage('AI Agent\'s'),
-    'boyunca': MessageLookupByLibrary.simpleMessage('by'),
-    'camera': MessageLookupByLibrary.simpleMessage('Camera'),
-    'card': MessageLookupByLibrary.simpleMessage('Cart'),
-    'categories': MessageLookupByLibrary.simpleMessage('Categories'),
-    'category': MessageLookupByLibrary.simpleMessage('Category'),
-    'category_empty': MessageLookupByLibrary.simpleMessage('No subcategory'),
-    'chats': MessageLookupByLibrary.simpleMessage('Chats'),
-    'derejani_galdyr': MessageLookupByLibrary.simpleMessage('Upgrade level'),
-    'gallery': MessageLookupByLibrary.simpleMessage('Gallery'),
-    'live': MessageLookupByLibrary.simpleMessage('Live'),
-    'maslahat_beryan_harytlary': MessageLookupByLibrary.simpleMessage('recommended products'),
-    'my_mbium': MessageLookupByLibrary.simpleMessage('My Mbium'),
-    'nasazlyk_yuze_cykdy': MessageLookupByLibrary.simpleMessage('An error occurred'),
-    'no': MessageLookupByLibrary.simpleMessage('No'),
-    'noDataAvailable': MessageLookupByLibrary.simpleMessage('No data available'),
-    'ondurijiler': MessageLookupByLibrary.simpleMessage('Shops'),
-    'products': MessageLookupByLibrary.simpleMessage('Products'),
-    'reels': MessageLookupByLibrary.simpleMessage('Reels'),
-    'refresh': MessageLookupByLibrary.simpleMessage('Refresh'),
-    'satys': MessageLookupByLibrary.simpleMessage('sales'),
-    'search': MessageLookupByLibrary.simpleMessage('Search...'),
-    'top': MessageLookupByLibrary.simpleMessage('Top'),
-    'welayatlar_boyunca': MessageLookupByLibrary.simpleMessage('Welaýatlar boýunça'),
-    'yes': MessageLookupByLibrary.simpleMessage('Yes')
-  };
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Ai_agent": MessageLookupByLibrary.simpleMessage("AI agent"),
+        "AppName": MessageLookupByLibrary.simpleMessage("Mbium"),
+        "add": MessageLookupByLibrary.simpleMessage("Add"),
+        "ahlisi": MessageLookupByLibrary.simpleMessage("All"),
+        "ai_agendyn": MessageLookupByLibrary.simpleMessage("AI Agent\'s"),
+        "ayakgaplar": MessageLookupByLibrary.simpleMessage("Footwear"),
+        "baha_goraglylygy":
+            MessageLookupByLibrary.simpleMessage("Price protection"),
+        "boyunca": MessageLookupByLibrary.simpleMessage("by"),
+        "camera": MessageLookupByLibrary.simpleMessage("Camera"),
+        "card": MessageLookupByLibrary.simpleMessage("Cart"),
+        "categories": MessageLookupByLibrary.simpleMessage("Categories"),
+        "category": MessageLookupByLibrary.simpleMessage("Category"),
+        "category_empty":
+            MessageLookupByLibrary.simpleMessage("No subcategory"),
+        "chats": MessageLookupByLibrary.simpleMessage("Chats"),
+        "derejani_galdyr":
+            MessageLookupByLibrary.simpleMessage("Upgrade level"),
+        "egin_esikler": MessageLookupByLibrary.simpleMessage("Clothing"),
+        "elektronika": MessageLookupByLibrary.simpleMessage("Electronics"),
+        "gallery": MessageLookupByLibrary.simpleMessage("Gallery"),
+        "gune_cenli": MessageLookupByLibrary.simpleMessage("Up to 60 days"),
+        "kitaplar": MessageLookupByLibrary.simpleMessage("Books"),
+        "live": MessageLookupByLibrary.simpleMessage("Live"),
+        "maslahat_beriyanler":
+            MessageLookupByLibrary.simpleMessage("Recommended"),
+        "maslahat_beriyanler_subtitle": MessageLookupByLibrary.simpleMessage(
+            "Products based on your selected clothing"),
+        "maslahat_beryan_harytlary":
+            MessageLookupByLibrary.simpleMessage("recommended products"),
+        "mbium_coin_bilen":
+            MessageLookupByLibrary.simpleMessage("Pay with MBIUM Coin"),
+        "mugt_dastawka": MessageLookupByLibrary.simpleMessage("Free delivery"),
+        "mugt_dastawka_mbium_coin": MessageLookupByLibrary.simpleMessage(
+            "Free delivery when paying with MBIUM Coin"),
+        "my_mbium": MessageLookupByLibrary.simpleMessage("My Mbium"),
+        "nasazlyk_yuze_cykdy":
+            MessageLookupByLibrary.simpleMessage("An error occurred"),
+        "no": MessageLookupByLibrary.simpleMessage("No"),
+        "noDataAvailable":
+            MessageLookupByLibrary.simpleMessage("No data available"),
+        "ondurijiler": MessageLookupByLibrary.simpleMessage("Shops"),
+        "oyuncaklar": MessageLookupByLibrary.simpleMessage("Toys"),
+        "products": MessageLookupByLibrary.simpleMessage("Products"),
+        "reels": MessageLookupByLibrary.simpleMessage("Reels"),
+        "refresh": MessageLookupByLibrary.simpleMessage("Refresh"),
+        "satys": MessageLookupByLibrary.simpleMessage("sales"),
+        "search": MessageLookupByLibrary.simpleMessage("Search..."),
+        "top": MessageLookupByLibrary.simpleMessage("Top"),
+        "welayatlar_boyunca":
+            MessageLookupByLibrary.simpleMessage("Welaýatlar boýunça"),
+        "yes": MessageLookupByLibrary.simpleMessage("Yes")
+      };
 }
