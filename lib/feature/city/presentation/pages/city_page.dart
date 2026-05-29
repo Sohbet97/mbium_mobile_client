c0150abb8c11c89a436521a57e18f54009564aeb
Jennet003 <hayalperestbiri39@gmail.com>
2026-05-29 10:35:42 +0500

    feat: city page widgets

---
+++ lib/feature/city/presentation/pages/city_page.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mbium_mobile_client/feature/category/bloc/category_bloc.dart';
+import 'package:mbium_mobile_client/feature/home/presentation/widget/search_widget.dart';
+import 'package:mbium_mobile_client/feature/products/bloc/product_bloc.dart';
+import 'package:mbium_mobile_client/feature/products/data/product_repository.dart';
+import 'package:mbium_mobile_client/feature/products/models/filter_model.dart';
+import 'package:mbium_mobile_client/feature/products/models/product_model.dart';
+import 'package:mbium_mobile_client/feature/city/presentation/widget/city_category_tabs_widget.dart';
+import 'package:mbium_mobile_client/feature/city/presentation/widget/city_horizontal_list_widget.dart';
+import 'package:mbium_mobile_client/feature/city/presentation/widget/city_maslahat_widget.dart';
+import 'package:mbium_mobile_client/feature/city/presentation/widget/city_banner_product_widget.dart';
+import '../../../../generated/l10n.dart';
+
+class CityPage extends StatefulWidget {
+  const CityPage({super.key});
+
+  @override
+  State<CityPage> createState() => _CityPageState();
+}
+
+class _CityPageState extends State<CityPage> {
+  final _searchController = TextEditingController();
+  final _scrollController = ScrollController();
+  final _productController = ScrollController();
+
+  late ProductBloc _productBloc;
+  late ProductBloc _maslahatBloc;
+  late ProductBloc _bannerProductBloc;
+
+  FilterModel _productFilter = const FilterModel();
+  final List<ProductModel> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<CategoryBloc>().add(
+          const LoadCategoriesEvent(isRefresh: false),
+        );
+
+    _productBloc = ProductBloc(repository: context.read<ProductRepository>());
+    _productBloc.add(LoadProducts(_productFilter));
+    _productController.addListener(_onScrollProduct);
+
+    _maslahatBloc = ProductBloc(repository: context.read<ProductRepository>());
+    _maslahatBloc.add(const LoadProducts(FilterModel(limit: 10)));
+
+    _bannerProductBloc =
+        ProductBloc(repository: context.read<ProductRepository>());
+    _bannerProductBloc.add(const LoadProducts(FilterModel(limit: 10)));
+  }
+
+  void _onScrollProduct() {
+    if (_productController.position.pixels >=
+        _productController.position.maxScrollExtent - 200) {
+      _productBloc.add(LoadMoreProducts());
+    }
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    _scrollController.dispose();
+    _productController.dispose();
+    _productBloc.close();
+    _maslahatBloc.close();
+    _bannerProductBloc.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      controller: _scrollController,
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: SearchWidget(
+              controller: _searchController,
+              onTapPhoto: () {},
+              onTapAudio: () {},
+              onTapAi: () {},
+              onSubmit: () {},
+            ),
+          ),
+          const SizedBox(height: 16),
+
+          BlocConsumer<ProductBloc, ProductState>(
+            bloc: _productBloc,
+            listener: (context, state) {
+              if (state is ProductLoaded) {
+                setState(() => _products.addAll(state.products));
+              }
+            },
+            builder: (context, state) {
+              return CityHorizontalListWidget(
+                productBloc: _productBloc,
+                scrollController: _productController,
+                products: _products,
+              );
+            },
+          ),
+          const SizedBox(height: 12),
+
+          CityCategoryTabsWidget(
+            onCategorySelected: (id) {
+              setState(() {
+                _productFilter = FilterModel(categoryId: id);
+                _products.clear();
+                _productBloc.add(LoadProducts(_productFilter));
+              });
+            },
+          ),
+          const SizedBox(height: 16),
+
+          BlocProvider.value(
+            value: _maslahatBloc,
+            child: const CityMaslahatWidget(),
+          ),
+          const SizedBox(height: 20),
+
+          BlocProvider.value(
+            value: _bannerProductBloc,
+            child: const CityBannerProductWidget(),
+          ),
+          const SizedBox(height: 24),
+        ],
+      ),
+    );
+  }
+}
--- lib/feature/shops/presentation/shop_detail_screen.dart
+++ lib/feature/shops/presentation/shop_detail_screen.dart
@@ -20,3 +20,4 @@ class _ShopDetailScreenState extends State<ShopDetailScreen> {
     );
   }
 }
+  
